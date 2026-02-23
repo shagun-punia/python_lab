1b1176507c5f5b6fc0f486cc01cb0341d24a2e86
shagun-punia <shagun.punia.mca.2025@miet.ac.in>
2026-02-23 19:02:11 +0530

    Delete pattern4.py

--- pattern4.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter rows"))
-if n>0:
-    for rows in range(1,n+1):
-        print(" "*(n-rows)+"*"*(2*rows-1))
-    for rows in range(n-1,0,-1):
-        print(" "*(n-rows)+"*"*(2*rows-1))
-
-else:
-     print("not possible")
