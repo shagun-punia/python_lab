7efb820dc6030d2dffd27489483d1bcea9b6d444
shagun-punia <shagun.punia.mca.2025@miet.ac.in>
2026-02-24 11:13:27 +0530

    Add files via upload

---
+++ 100_Python_Programs_24_Feb_26/6_convert_temp_from_celsius_to_fahrenheit.py
@@ -0,0 +1,18 @@
+# Program to convert temperature from Celsius to Fahrenheit
+
+# Taking input from user
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Converting Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Displaying the result
+print("\n----- Output -----")
+print("Temperature in Celsius:", celsius)
+print("Temperature in Fahrenheit:", fahrenheit)
+#OUTPUT
+"""Enter temperature in Celsius: 4
+
+----- Output -----
+Temperature in Celsius: 4.0
+Temperature in Fahrenheit: 39.2"""
