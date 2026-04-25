72d633a469b8886970130df55bb82f1211c0c60c
Shagun Punia <shagun.punia.mca.2025@miet.ac.in>
2026-04-25 10:41:07 +0530

    Delete Arithmetic_operator directory

--- Arithmetic_operator/celsius_to_fahrenheit.py
+++
@@ -1,4 +0,0 @@
-c = float(input("Enter temperature in Celsius: "))
-
-f = (c * 9/5) + 32
-print("Temperature in Fahrenheit =", f)
--- Arithmetic_operator/division.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-
-if b != 0:
-    print("Quotient =", a / b)
-else:
-    print("Division by zero not allowed")
